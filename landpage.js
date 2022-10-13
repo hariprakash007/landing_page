d52ba5310b74db653b92d8370628aecdca9fae57
Hari Prakash <hariprakash35555@gmail.com>
2022-10-13 17:10:04 +0530

    test commit

--- landpage.js
+++ landpage.js
@@ -1,6 +1,8 @@
 const menuToggle = document.querySelector(".toggle");
 const showcase = document.querySelector(".showcase");
 
+console.log("Hi");
+
 menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
   showcase.classList.toggle("active");
